d9ca7803c74f6d07c9e8aca934440b28bd44c3fd
phudyka <paul.hudyka@gmail.com>
2024-05-29 17:06:23 +0200

    ok

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -7,23 +7,23 @@ if [ -f /var/www/wordpress/wp-config.php ]; then
 else
     echo "Creating wp-config.php"
     wp config create --allow-root \
-                     --dbname="$MYSQL_DATABASE" \
-                     --dbuser="$MYSQL_USER" \
-                     --dbpass="$MYSQL_PASSWORD" \
+                     --dbname="${MYSQL_DATABASE}" \
+                     --dbuser="${MYSQL_USER}" \
+                     --dbpass="${MYSQL_PASSWORD}" \
                      --dbhost="mariadb:3306" \
                      --path='/var/www/wordpress'
 
     wp core install --allow-root \
-                    --url="$WORDPRESS_URL" \
-                    --title="$WORDPRESS_TITLE" \
-                    --admin_user="$WORDPRESS_ADMIN_USER" \
-                    --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-                    --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+                    --url="${WORDPRESS_URL}" \
+                    --title="${WORDPRESS_TITLE}" \
+                    --admin_user="${WORDPRESS_ADMIN_USER}" \
+                    --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+                    --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
                     --path='/var/www/wordpress'
 
-    wp user create "$WORDPRESS_ADMIN_USER" "$WORDPRESS_ADMIN_EMAIL" --role=author --user_pass="$WORDPRESS_ADMIN_PASSWORD" --path='/var/www/wordpress' --allow-root
+    wp user create "${WORDPRESS_ADMIN_USER}" "${WORDPRESS_ADMIN_EMAIL}" --role=author --user_pass="${WORDPRESS_ADMIN_PASSWORD}" --path='/var/www/wordpress' --allow-root
 fi
 
 mkdir -p /run/php
 
-/usr/sbin/php-fpm7.3 -F
+exec /usr/sbin/php-fpm7.3 -F
